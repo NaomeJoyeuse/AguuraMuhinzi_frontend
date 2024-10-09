a83c25f654a2dab5ca5d4542eb3d080d93204fa6
NaomeJoyeuse <naomejoyce5@gmail.com>
2024-10-09 17:35:40 +0200

    landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Signup from './pages/test_page.tsx';  
+import Home from './pages/landingPage.tsx';
+import Choose from './pages/choose.tsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} /> 
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/choose" element={<Choose />} />
+       
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
